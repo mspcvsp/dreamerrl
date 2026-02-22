c3f1c210c5aafe3fd6e3861413eafe208c09ebd4
Sean McCarty <mspcvsp@gmail.com>
2026-02-22 14:12:06 -0500

    Removed LSTM PPO tests so that I can start building dreamer tests

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import torch
-
-
-def pytest_runtest_setup(item):
-    if "gpu" in item.keywords and not torch.cuda.is_available():
-        pytest.skip("Skipping GPU test because CUDA is not available")
