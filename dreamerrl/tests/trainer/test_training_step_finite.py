a53fb16882bc65109fe8d86995120643992d47ea
Sean McCarty <mspcvsp@gmail.com>
2026-04-12 15:45:11 -0400

    Updated

--- dreamerrl/tests/trainer/test_training_step_finite.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-def test_training_step_finite(test_trainer):
-    out = test_trainer.training_step(batch_size=4, seq_len=5)
-    assert torch.isfinite(out["world_model_loss"])
-    assert torch.isfinite(out["actor_loss"])
-    assert torch.isfinite(out["critic_loss"])
